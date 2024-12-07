93d03e454a3379c3f2c6446fdd98566bb6edf5bc
Basanta-khatri-0311 <khatribasanta.git@gmail.com>
2024-12-07 23:43:31 +0545

    learning the data types in js

---
+++ clock/jspractice/script.js
@@ -0,0 +1,32 @@
+// 1.Variables
+// These are how a variable gets their value and which can be changed and which cannot be changed
+
+// const myName = "Basanta"; .....> This variable cannot be reassigned since its decleared as const 
+// const age = 22; .....> This variable cannot be reassigned since its decleared as const 
+// let favHobby = "Playing Guitar"; .....> This variable can be reassigned since its decleared as let  
+
+// myName += "Khatri";
+// favHobby += " PLaying Cricket";
+// console.log(favHobby)
+
+
+// 2.Data Types
+
+// const myName = "Basanta"
+// let age = 22
+// let isAlive = true
+// const dob = null
+// const myTopThreeLanguages = ['javascript','python','R']
+// const car = {
+//     brand:"BMW",
+//     model: "X5",
+//     year:2022,
+//     owner: {  //Nested object inside 1 object
+//         name:'Basanta',
+//         age:22,
+//         city:"Butwal"
+//     }
+// }
+
+// console.log(`I know a friend his name is ${car.owner.name} from ${car.owner.city}.
+//     He is ${car.owner.age} years old. He has a ${car.brand} ${car.model} of ${car.year}`)
